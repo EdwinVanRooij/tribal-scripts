79067c2e39874d66cbd653f0e1acb1d1d19fbe7a
Edwin van Rooij <edwinvrooij@gmail.com>
2017-07-30 17:51:18 +0200

    not working rn

--- tribot.js
+++ tribot.js
@@ -1,44 +1,46 @@
+var wood = "wood";
+var stone = "stone";
+var iron = "iron";
+
+function getStock(type) {
+    return document.getElementById("premium_exchange_stock_" + type).innerHTML;
+}
+
+function getInputField(type) {
+     return document.getElementById("premium_exchange_buy_" + type).getElementsByClassName("premium-exchange-input")[0];
+}
+function getRate(type) {
+    return document.getElementById("premium_exchange_rate_" + type).getElementsByClassName("premium-exchange-sep")[0].textContent.replace(/\s/g, '');
+}
+
+function setValue(stock, rate, inputField) {
+    if (stock >= rate) {
+        var remaining = stock % rate;
+        inputField.value = stock - remaining;
+        return true;
+    } else {
+        inputField.value = "";
+        return false;
+    }
+}
+
 function run() {
-    var wood = document.getElementById("premium_exchange_stock_wood").innerHTML;
-    var stone = document.getElementById("premium_exchange_stock_stone").innerHTML;
-    var iron = document.getElementById("premium_exchange_stock_iron").innerHTML;
+    var woodStock = getStock(wood);
+    var woodInput = getInputField(wood);
+    var woodRate = getRate(wood);
+    var woodResult = setValue(woodStock, woodRate, woodInput);
 
-    var woodInput = document.getElementById("premium_exchange_buy_wood").getElementsByClassName("premium-exchange-input")[0];
-    var stoneInput = document.getElementById("premium_exchange_buy_stone").getElementsByClassName("premium-exchange-input")[0];
-    var ironInput = document.getElementById("premium_exchange_buy_iron").getElementsByClassName("premium-exchange-input")[0];
+    var stoneStock = getStock(stone);
+    var stoneRate = getRate(stone);
+    var stoneInput = getInputField(stone);
+    var stoneResult = setValue(stoneStock, stoneRate, stoneInput);
 
-    var atleastOne = false;
+    var ironStock = getStock(iron);
+    var ironRate = getRate(iron);
+    var ironInput = getInputField(iron);
+    var ironResult = setValue(ironStock, ironRate, ironInput);
 
-    var price = 64;
-    var price = document.getElementById("premium_exchange_rate_wood").getElementsByClassName("premium-exchange-input")[0];
-    if (wood >= price) {
-        var remaining = wood % price;
-        var numberToInput = wood - remaining;
-        woodInput.value = numberToInput;
-        atleastOne = true;
-    } else {
-        woodInput.value = 0;
-    }
-
-    if (stone >= price) {
-        var remaining = stone % price;
-        var numberToInput = stone - remaining;
-        stoneInput.value = numberToInput;
-        atleastOne = true;
-    } else {
-        stoneInput.value = 0;
-    }
-
-    if (iron >= price) {
-        var remaining = iron % price;
-        var numberToInput = iron - remaining;
-        ironInput.value = numberToInput;
-        atleastOne = true;
-    } else {
-        ironInput.value = 0;
-    }
-
-    if (atleastOne) {
+    if (woodResult || stoneResult || ironResult) {
         setTimeout(function () {
             clickButtons();
         }, 500);
